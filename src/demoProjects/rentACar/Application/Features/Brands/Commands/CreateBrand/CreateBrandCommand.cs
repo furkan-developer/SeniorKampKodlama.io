c6248f0df5101c50373f5a573726cbe58170dde3
Furkan Aydin <furkan.aydin@intern.veripark.com>
2022-08-24 09:25:51 +0300

    Brand command process created

---
+++ src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommand.cs
@@ -0,0 +1,19 @@
+﻿using Application.Features.Brands.Dtos;
+using Application.Features.Brands.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Commands.CreateBrand
+{
+    public class CreateBrandCommand:IRequest<CreatedBrandDto>
+    {
+        public string Name { get; set; }       
+    }
+}
