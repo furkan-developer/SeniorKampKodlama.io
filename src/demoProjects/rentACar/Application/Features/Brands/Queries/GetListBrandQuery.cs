12b9e82508912dac6c901eea9f8c312aadc37746
Furkan Aydin <furkan.aydin@intern.veripark.com>
2022-09-02 10:24:47 +0300

    Created Brand Get List Query

---
+++ src/demoProjects/rentACar/Application/Features/Brands/Queries/GetListBrandQuery.cs
@@ -0,0 +1,20 @@
+﻿using Application.Features.Brands.Models;
+using Application.Services.Repositories;
+using AutoMapper;
+using Core.Application.Requests;
+using Core.Persistence.Paging;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Queries
+{
+    public class GetListBrandQuery:IRequest<BrandListModel>
+    {
+        public PageRequest PageRequest { get; set; }       
+    }
+}
