df99b6d58a9962b32cb6dedd55d655a3247e05a6
Furkan Aydin <furkan.aydin@intern.veripark.com>
2022-09-29 21:12:03 +0300

    Add Model entity

--- src/demoProjects/rentACar/Persistence/Contexts/BaseDbContext.cs
+++ src/demoProjects/rentACar/Persistence/Contexts/BaseDbContext.cs
@@ -13,7 +13,8 @@ namespace Persistence.Contexts
     {
         protected IConfiguration Configuration { get; set; }
         public DbSet<Brand> Brands { get; set; }
-       
+        public DbSet<Model> Models { get; set; }
+
 
         public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
         {
@@ -34,14 +35,27 @@ namespace Persistence.Contexts
                 a.ToTable("Brands").HasKey(k => k.Id);
                 a.Property(p => p.Id).HasColumnName("Id");
                 a.Property(p => p.Name).HasColumnName("Name");
+
+                a.HasMany(p => p.Models);
             });
 
+            modelBuilder.Entity<Model>(a =>
+            {
+                a.ToTable("Models").HasKey(k => k.Id);
+                a.Property(p => p.Id).HasColumnName("Id");
+                a.Property(p => p.Name).HasColumnName("Name");
+                a.Property(p => p.DailyPrice).HasColumnName("DailyPrice");
+                a.Property(p => p.ImageUrl).HasColumnName("ImageUrl");
+                a.Property(p => p.BrandId).HasColumnName("BrandId");
 
+                a.HasOne(p => p.Brand);
+            });
 
             Brand[] brandEntitySeeds = { new(1, "BMW"), new(2, "Mercedes") };
             modelBuilder.Entity<Brand>().HasData(brandEntitySeeds);
 
-           
+            Model[] ModelEntitySeeds = { new(1, 1, "Model 1", 2500, ""), new(2, 1, "Model 2", 2500, ""), new(3, 2, "Model 3", 2500, "") };
+            modelBuilder.Entity<Model>().HasData(ModelEntitySeeds);
         }
     }
 }
