2023dd90df69de7c899554b619ccb643f828acb1
Furkan Aydin <furkan.aydin@intern.veripark.com>
2022-10-01 12:24:13 +0300

    Added process of GetListModelQuery ( Include Brand )

---
+++ src/demoProjects/rentACar/Application/Features/Models/Profiles/MappingProfiles.cs
@@ -0,0 +1,22 @@
+﻿using Application.Features.Models.Dtos;
+using Application.Features.Models.Models;
+using AutoMapper;
+using Core.Persistence.Paging;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Models.Profiles
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Model, ModelListDto>().ForMember(d => d.BrandName, opt => opt.MapFrom(s => s.Brand)).ReverseMap();
+            CreateMap<IPaginate<Model>, ModelListModel>().ReverseMap();
+        }
+    }
+}
---
+++ src/demoProjects/rentACar/WebAPI/Controllers/ModelsController.cs
@@ -0,0 +1,21 @@
+﻿using Application.Features.Models.Models;
+using Application.Features.Models.Queries;
+using Core.Application.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModelsController : BaseController
+    {
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAll([FromQuery] PageRequest pageRequest)
+        {
+            GetListModelQuery getListModelQuery = new() { PageRequest = pageRequest };
+            ModelListModel result = await Mediator.Send(getListModelQuery);
+            return Ok(result);
+        }
+    }
+}
